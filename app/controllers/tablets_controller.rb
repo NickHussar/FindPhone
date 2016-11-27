4af39588aaace0d9a8a8a38b5f8702362f67553b
Plisskeens <koteleta@gmail.com>
2016-11-27 23:24:58 +0300

    App V2

--- app/controllers/tablets_controller.rb
+++ app/controllers/tablets_controller.rb
@@ -19,6 +19,6 @@ class TabletsController < ApplicationController
 
   private
     def tablet_params
-      params.require(:tablet).permit(:developer, :manufacturer, :model, :description, :year, :resolution, :diagonal, :screen_type, :dpi, :multitouch, :cpu, :cores, :frequency, :gpu, :inner_mem, :access_mem, :ram, :cam, :megapixels, :photo_res, :hdr, :front_megapixels, :video_res, :fps, :four_k_video, :calls, :sms, :mms, :three_g, :four_g, :wifi, :bluetooth, :nfc, :gps, :gyroscope, :accelerometer, :barometer, :wireless_charge, :fast_charge, :stylus, :keyboard, :battery, :standby_time, :talk_time, :os, :color, :weight, :dimensions, :material, :protection )
+      params.require(:tablet).permit(:developer, :manufacturer, :model, :description, :year, :resolution, :diagonal, :screen_type, :dpi, :multitouch, :cpu, :cores, :frequency, :gpu, :inner_mem, :access_mem, :ram, :cam, :megapixels, :photo_res, :hdr, :front_megapixels, :video_res, :fps, :four_k_video, :calls, :sms, :mms, :three_g, :four_g, :wifi, :bluetooth, :nfc, :gps, :gyroscope, :accelerometer, :barometer, :wireless_charge, :fast_charge, :stylus, :keyboard, :battery, :standby_time, :talk_time, :os, :color, :weight, :dimensions, :material, :protection)
     end
 end
--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -19,6 +19,6 @@ class WatchesController < ApplicationController
 
   private
     def watch_params
-      params.require(:watch).permit(:developer, :manufacturer, :model, :description, :year, :resolution, :diagonal, :screen_type, :color_screen, :dpi, :touchscreen, :multitouch, :cpu, :cores, :frequency, :gpu, :inner_mem, :access_mem, :ram, :cam, :megapixels, :photo_res, :autofocus, :video_rec, :video_res, :fps, :finder, :calls, :email, :sms, :mms, :wifi, :bluetooth, :nfc, :gps, :gyroscope, :accelerometer, :barometer, :thermometer, :compass, :chronograph, :pedometer, :pulse_sensor, :fitness_tracker, :wireless_charge, :fast_charge, :battery, :standby_time, :clock_time, :os, :online_service, :compatibility, :speaker, :mic, :color, :weight, :dimensions, :material, :protection )
+      params.require(:watch).permit(:developer, :manufacturer, :model, :description, :year, :resolution, :diagonal, :screen_type, :color_screen, :dpi, :touchscreen, :multitouch, :cpu, :cores, :frequency, :gpu, :inner_mem, :access_mem, :ram, :cam, :megapixels, :photo_res, :autofocus, :video_rec, :video_res, :fps, :finder, :calls, :email, :sms, :mms, :wifi, :bluetooth, :nfc, :gps, :gyroscope, :accelerometer, :barometer, :thermometer, :compass, :chronograph, :pedometer, :pulse_sensor, :fitness_tracker, :wireless_charge, :fast_charge, :battery, :standby_time, :clock_time, :os, :online_service, :compatibility, :speaker, :mic, :color, :weight, :dimensions, :material, :protection)
     end
 end
